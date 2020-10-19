49d0c3f828eb762edc1a7195e85d8e3778d9b502
lvlcnapps <57109390+lvlcnapps@users.noreply.github.com>
2020-10-19 05:05:15 +0300

    background, enemies (just looking at player)

--- govnogovnaisgovna/govnogovnaisgovna/main.cpp
+++ govnogovnaisgovna/govnogovnaisgovna/main.cpp
@@ -2,12 +2,33 @@
 #include <iostream>
 #include <math.h>
 #include <ctime>
+#include <fstream>
 
 #define pi 3.1415
 
 using namespace std;
 
-float playerSpeed = 4, bulletSpeed = 12;
+/*int map1[15][20] {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};*/
+int map1[15][20];
+
+float playerSpeed = 2, bulletSpeed = 6;
+int hp = 100, ammo = 30;
 
 int rotation(int mouse_x, int mouse_y, int pl_x, int pl_y) {
     return atan2((mouse_y - pl_y), (mouse_x - pl_x)) / pi * 180;
@@ -19,45 +40,90 @@ struct bulletO{
 };
 
 int main() {
+    ifstream fin("map.m");
+    for (int o = 0; o < 15; o++) {
+        for (int e = 0; e < 20; e++) {
+            fin >> map1[o][e];
+            if (map1[o][e] == 1) {
+                cout << "Block\n";
+            }
+        }
+    }
+    fin.close();
     float pl_x = 0, pl_y = 0;
     clock_t flagTime = clock();
-    int can_shoot = 1, timer = 0, last_time = -1, cooldownShoot = 200;
+    int can_shoot = 1, timer = 0, last_time = -1, cooldownShoot = 50;
     vector <bulletO> bullets;
-    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(640, 480), "TDS");
     sf::Texture texture;
     if (!texture.loadFromFile("player.png")) {
+        cout << "5";
+    }
+    sf::Texture enemyT;
+    if (!enemyT.loadFromFile("enemy.png")) {
         cout << "5";
     }
     sf::Texture bulletT;
     if (!bulletT.loadFromFile("bullet.png")) {
         cout << "5";
     }
+    sf::Texture backGT;
+    if (!backGT.loadFromFile("background1.png")) {
+        cout << "5";
+    }
+    sf::Texture blockT;
+    if (!blockT.loadFromFile("wall.png")) {
+        cout << "5";
+    }
+
+    for (int o = 0; o < 15; o++) {
+        for (int e = 0; e < 20; e++) {
+            switch (map1[o][e]) {
+                case 1:
+                    sf::Sprite blockS;
+                    blockS.setTexture(blockT);
+                    blockS.setPosition(e * 32, o * 32);
+                    //blockS.setPosition(0, 0);
+                    window.draw(blockS);
+            }
+        }
+    }
+
     bulletT.setSmooth(true);
 
     sf::Sprite sprite;
     sprite.setTexture(texture);
     sprite.setOrigin(sf::Vector2f(13.f, 13.f));
 
-    /*sf::Sprite bulletS;
-    bulletS.setTexture(bulletT);
-    bulletS.setOrigin(sf::Vector2f(30.f, 12.f));*/
+    sf::Sprite backS;
+    backS.setTexture(backGT);
+
+    sf::View view(sf::FloatRect(0.f, 0.f, 640.f, 480.f));
+    window.setView(view);
 
     while (window.isOpen()) {
         clock_t nowTime = clock();
-        cout << nowTime << endl;
-        if (nowTime - flagTime > 20) {
+        //cout << nowTime << endl;
+        if (nowTime - flagTime > 10) {
+            int fps = 1000 / (nowTime - flagTime);
+            //cout << fps << endl;
             flagTime = nowTime;
             sf::Event event;
             while (window.pollEvent(event)) {
                 if (event.type == sf::Event::Closed)
                     window.close();
+                if (event.type == sf::Event::Resized)
+                {
+                    sf::FloatRect visibleArea(0.f, 0.f, event.size.width, event.size.height);
+                    window.setView(sf::View(visibleArea));
+                }
             }
 
             sf::Vector2i localPosition = sf::Mouse::getPosition(window);
             sprite.setRotation(rotation(localPosition.x, localPosition.y, pl_x, pl_y));
             //cout << rotation(localPosition.x, localPosition.y, pl_x, pl_y) << " " << pl_x << " " << pl_y << endl;
 
-            if ((can_shoot == 1) && (sf::Mouse::isButtonPressed(sf::Mouse::Left))) {
+            if ((ammo > 0) && (can_shoot == 1) && (sf::Mouse::isButtonPressed(sf::Mouse::Left))) {
                 bulletO bul;
                 bul.x = pl_x;
                 bul.y = pl_y;
@@ -65,6 +131,7 @@ int main() {
                 bul.created = 1;
                 bullets.push_back(bul);
                 can_shoot = 0;
+                ammo--;
                 cout << "6";
                 last_time = timer;
             }
@@ -75,6 +142,15 @@ int main() {
                 can_shoot = 1;
                 last_time = -1;
                 timer = 0;
+            }
+            
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
+                view.move(100.f, 0.f);
+                cout << "Z\n";
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::X)) {
+                view.rotate(5.f);
+                cout << "X\n";
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
                 sprite.move(sf::Vector2f(-playerSpeed, 0.f));
@@ -87,6 +163,11 @@ int main() {
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
                 sprite.move(sf::Vector2f(0.f, -playerSpeed));
                 pl_y -= playerSpeed;
+                /*if (sprite.getGlobalBounds().intersects(blockS.getGlobalBounds())) {
+                    sprite.move(sf::Vector2f(0.f, playerSpeed));
+                    pl_y += playerSpeed;
+                    cout << "Collision\n";
+                }*/
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
                 sprite.move(sf::Vector2f(0.f, playerSpeed));
@@ -102,19 +183,30 @@ int main() {
             }
 
             window.clear();
+            window.draw(backS);
+            for (int o = 0; o < 15; o++) {
+                for (int e = 0; e < 20; e++) {
+                    if (map1[o][e] == 1) {
+                        sf::Sprite blockS;
+                        blockS.setTexture(blockT);
+                        blockS.setPosition(e * 32, o * 32);
+                        window.draw(blockS);
+                    }
+                    if (map1[o][e] == 2) {
+                        sf::Sprite enemyS;
+                        enemyS.setTexture(enemyT);
+                        enemyS.setOrigin(sf::Vector2f(19.f, 13.f));
+                        enemyS.setPosition(e * 32, o * 32);
+                        enemyS.setRotation(rotation(e * 32, o * 32, pl_x, pl_y) + 180);
+                        window.draw(enemyS);
+                    }
+                }
+            }
             window.draw(sprite);
             for (bulletO& i : bullets) {
                 sf::Sprite bulletSP;
                 bulletSP.setTexture(bulletT);
-                bulletSP.setOrigin(sf::Vector2f(30.f, 12.f));
-                //cout << i.created << endl;
-                /*if (i.created == 1) {
-                    bulletSP.setPosition(i.x, i.y);
-                    i.created = 0;
-                    //cout << "7";
-                } else {
-                    bulletSP.move(cos(i.angle) * bulletSpeed, sin(i.angle) * bulletSpeed);
-                }*/
+                bulletSP.setOrigin(sf::Vector2f(10.f, 12.f));
                 bulletSP.setPosition(i.x, i.y);
                 i.x += cos(i.angle / 180 * pi) * bulletSpeed;
                 i.y += sin(i.angle / 180 * pi) * bulletSpeed;
